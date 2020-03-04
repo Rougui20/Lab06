ff11c0aa2cde40027d82e55b72e927f800cc8a4f
Rouguiatou Traore <rouguiatoutraore@Rouguiatous-MacBook-Air.local>
2020-03-04 16:42:30 -0500

    second commit

---
+++ lab06001.py
@@ -0,0 +1,19 @@
+class Node():#create the attributs
+	def __init__(self,data,nextPointer):
+		self.data = data
+		self.nextPointer = nextPointer
+		#put the getters and the setters
+		def set_data(self,d):
+			self.data = d
+		def get_data(self,data):
+			return(self.data)
+
+		def set_nextPointer(self,n):
+			self.nextPointer = n
+		def get_nextPointer(self,n):
+			return(self.nextPointer)
+def main():
+
+
+	if __name__ == '__main__':
+		main()
